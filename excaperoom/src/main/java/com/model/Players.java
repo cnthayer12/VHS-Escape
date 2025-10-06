e31c019374d3fb773745efa096106fa1ff81a80a
Bianca Zeigler <bzeigler@Biancas-MacBook-Pro.local>
2025-10-05 20:38:07 -0400

    players.java

--- excaperoom/src/main/java/com/model/Players.java
+++ excaperoom/src/main/java/com/model/Players.java
@@ -1,57 +1,52 @@
 package com.model;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class Players {
-	private static Players players;
-	private ArrayList<Player> playerList;
-	
-	private Players() {
-		playerList = DataLoader.getPlayers();
-	}
-	
-	public static Players getInstance() {
-		if(players == null) {
-			players = new Players();
-		}
-		
-		return players;
-	}
 
-	public boolean havePlayer(String displayName) {
-		for(Player player : playerList) {
-			if(player.getDisplayName()().equals(displayName)) {
-				return true;
-			}
-		}
-		
-		return false;
-	}
-	
-	public User getUser(String userName) {
-		for(User user : userList) {
-			if(user.getUserName().equals(userName)) {
-				return user;
-			}
-		}
-		
-		return null;
-	}
-	
-	public ArrayList<User> getUsers() {
-		return userList;
-	}
-	
-	public boolean addUser(String userName, String firstName, String lastName, int age, String phoneNumber) {
-		if(haveUser(userName))return false;
-		
-		userList.add(new User(userName, firstName, lastName, age, phoneNumber));
-		return true;
-	}
-	
-	public void saveUsers() {
-		DataWriter.saveUsers();
-	}
-}
-    
+    private static Players instance = null;
+
+    private ArrayList<Player> players;
+
+    private Players() {
+        players = new ArrayList<>();
+    }
+
+    public static Players getInstance() {
+        if (instance == null) {
+            instance = new Players();
+        }
+        return instance;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(ArrayList<Player> players) {
+        this.players = players;
+    }
+
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    public Player getPlayer(UUID id) {
+        for (Player player : players) {
+            if (player.getId().equals(id)) {
+                return player;
+            }
+        }
+        return null;
+    }
+
+    public boolean removePlayer(UUID id) {
+        Player player = getPlayer(id);
+        if (player != null) {
+            players.remove(player);
+            return true;
+        }
+        return false;
+    }
 }
