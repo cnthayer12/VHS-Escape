ae9f0a77b26b0d712c25815b1620becd2ae21434
cnthayer12 <cnthayer@email.sc.edu>
2025-10-19 20:39:30 -0400

    created 5 scenerios and tester

--- excaperoom/src/main/java/com/model/Item.java
+++ excaperoom/src/main/java/com/model/Item.java
@@ -1,5 +1,131 @@
 package com.model;
 
+import java.io.File;
+
 public class Item {
+    private String name;
+    private String description;
+    private String location;
+    private File sound;
+
+    public Item() {
+        this.name = "";
+        this.description = "";
+        this.location = "";
+        this.sound = null;
+    }
+
+    public Item(String name, String description, String location, File sound) {
+        this.name = name;
+        this.description = description;
+        this.location = location;
+        this.sound = sound;
+    }
+
+    public boolean use(Puzzle puzzle) {
+        if (puzzle == null) {
+            System.out.println("Cannot use " + name + " - no puzzle selected");
+            return false;
+        }
+        
+        System.out.println("Using " + name + " on puzzle");
+        
+        if (sound != null && sound.exists()) {
+            playSound();
+        }
+        
+        return true;
+    }
+
+    public boolean addToInventory() {
+        System.out.println(name + " added to inventory");
+        
+        if (sound != null && sound.exists()) {
+            playSound();
+        }
+        
+        return true;
+    }
     
+    public boolean addToInventory(Progress progress) {
+        if (progress == null) {
+            System.out.println("Error: Progress is null");
+            return false;
+        }
+        
+        if (progress.getInventory() == null) {
+            System.out.println("Error: Inventory not initialized");
+            return false;
+        }
+        
+        progress.getInventory().add(this);
+        System.out.println(name + " added to inventory");
+        
+        if (sound != null && sound.exists()) {
+            playSound();
+        }
+        
+        return true;
+    }
+
+    private void playSound() {
+        System.out.println("STUB: Playing sound for item: " + name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public File getSound() {
+        return sound;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setSound(File sound) {
+        this.sound = sound;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", location='" + location + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        
+        Item item = (Item) obj;
+        return name.equals(item.name) && location.equals(item.location);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (location != null ? location.hashCode() : 0);
+        return result;
+    }
 }
--- excaperoom/src/main/java/com/model/Test.java
+++ excaperoom/src/main/java/com/model/Test.java
@@ -32,5 +32,57 @@ public class Test {
 
         System.out.println("\nLogout scenario, successful:");
         players.logout();
+
+        // Puzzle Tests
+        System.out.println("\n========== PUZZLE TESTS ==========");
+        
+        PuzzlesManager manager = PuzzlesManager.getInstance();
+        manager.addPuzzle(PuzzleFactory.createVHSMultipleChoice());
+        manager.addPuzzle(PuzzleFactory.createTVStaticPixelHunt());
+        manager.addPuzzle(PuzzleFactory.createRemoteCipher());
+        manager.addPuzzle(PuzzleFactory.create80sTrivia());
+        manager.addPuzzle(PuzzleFactory.createVHSRiddle());
+        
+        System.out.println("\nMultiple choice puzzle, wrong answer:");
+        MultipleChoicePuzzle mc = (MultipleChoicePuzzle) manager.getPuzzles().get(0);
+        System.out.println(mc.displayQuestion());
+        System.out.println("Answer 'Rewind the tape': " + mc.checkAnswer("Rewind the tape"));
+        
+        System.out.println("\nMultiple choice puzzle, correct answer:");
+        System.out.println("Answer 'Pause at 13:37': " + mc.checkAnswer("Pause at 13:37"));
+        
+        System.out.println("\nPixel hunt puzzle, wrong answer:");
+        PixelHuntPuzzle ph = (PixelHuntPuzzle) manager.getPuzzles().get(1);
+        System.out.println(ph.displayGrid());
+        System.out.println("Answer 'ABC': " + ph.checkAnswer("ABC"));
+        
+        System.out.println("\nPixel hunt puzzle, correct answer:");
+        System.out.println("Answer 'VHS': " + ph.checkAnswer("VHS"));
+        
+        System.out.println("\nCipher puzzle, wrong answer:");
+        CipherPuzzle cp = (CipherPuzzle) manager.getPuzzles().get(2);
+        System.out.println(cp.displayPuzzle());
+        System.out.println("Answer 'CHANNEL TEN': " + cp.checkAnswer("CHANNEL TEN"));
+        
+        System.out.println("\nCipher puzzle, correct answer:");
+        System.out.println("Answer 'CHANNEL THIRTEEN': " + cp.checkAnswer("CHANNEL THIRTEEN"));
+        
+        System.out.println("\nTrivia puzzle, wrong answer:");
+        TriviaPuzzle tp = (TriviaPuzzle) manager.getPuzzles().get(3);
+        System.out.println(tp.displayQuestion());
+        System.out.println("Answer 'The Terminator': " + tp.checkAnswer("The Terminator"));
+        
+        System.out.println("\nTrivia puzzle, correct answer:");
+        System.out.println("Answer 'Back to the Future': " + tp.checkAnswer("Back to the Future"));
+        
+        System.out.println("\nRiddle puzzle, wrong answer:");
+        RiddlePuzzle rp = (RiddlePuzzle) manager.getPuzzles().get(4);
+        System.out.println(rp.displayRiddle());
+        System.out.println("Answer 'clock': " + rp.checkAnswer("clock"));
+        
+        System.out.println("\nRiddle puzzle, correct answer:");
+        System.out.println("Answer 'tape': " + rp.checkAnswer("tape"));
     }
+
+    
 }
