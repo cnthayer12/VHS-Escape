8cbc6e5bbff42471c86ac141db42a00a1d516b69
Charlotte Baker <crb27@email.sc.edu>
2025-10-26 14:23:20 -0400

    Fix

--- excaperoom/src/main/java/com/model/Driver.java
+++ excaperoom/src/main/java/com/model/Driver.java
@@ -1,5 +1,12 @@
 package com.model;
 
 public class Driver {
+    private EscapeGameFacade facade;
+    private Players players;
+    
+    public Driver() {
+        this.facade = EscapeGameFacade.getInstance();
+        this.players = Players.getInstance();
+    }
     
 }
