6e925309c1dbf2abddda645be06becb98dbd9d82
l-ukin <schoolwork.lukin@gmail.com>
2025-11-18 14:25:59 -0500

    landing update

--- excaperoom/src/main/java/com/excape/LandingController.java
+++ excaperoom/src/main/java/com/excape/LandingController.java
@@ -2,12 +2,45 @@ package com.excape;
 
 import java.io.IOException;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 public class LandingController {
 
     @FXML
-    private void switchToLogin() throws IOException {
+    private VBox bgLanding;
+
+    @FXML
+    private Button btnCreateAcc;
+
+    @FXML
+    private Button btnLeaderboard;
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private Label lblTitle;
+
+    @FXML
+    private Label lblWelcome;
+
+    @FXML
+    void switchToCreateAcc(ActionEvent event) throws IOException {
+        App.setRoot("createAcc");
+    }
+
+    @FXML
+    void switchToLeaderboard(ActionEvent event) throws IOException {
+        App.setRoot("leaderboard");
+    }
+
+    @FXML
+    void switchToLogin(ActionEvent event) throws IOException {
         App.setRoot("login");
     }
+
 }
