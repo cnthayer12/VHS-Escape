554eb3c594865a2916493dd21bf5c4e87f92707f
l-ukin <schoolwork.lukin@gmail.com>
2025-11-19 01:33:46 -0500

    login screen (unfinished)

--- excaperoom/src/main/java/com/excape/LoginController.java
+++ excaperoom/src/main/java/com/excape/LoginController.java
@@ -3,11 +3,60 @@ package com.excape;
 import java.io.IOException;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Line;
 
 public class LoginController {
 
     @FXML
-    private void switchToLanding() throws IOException {
+    private Label backArrow;
+
+    @FXML
+    private Label createAccLink;
+
+    @FXML
+    private Label donthaveLabel;
+
+    @FXML
+    private Label insteadLabel;
+
+    @FXML
+    private Line line;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Label loginLabel;
+
+    @FXML
+    private TextField pwInput;
+
+    @FXML
+    private Label pwLabel;
+
+    @FXML
+    private TextField unInput;
+
+    @FXML
+    private Label unLabel;
+
+    @FXML
+    void attemptLogin(MouseEvent event) throws IOException {
+
+    }
+
+    @FXML
+    void switchToCreateAcc(MouseEvent event) throws IOException {
+        App.setRoot("createAccount");
+    }
+
+    @FXML
+    void switchToLanding(MouseEvent event) throws IOException {
         App.setRoot("landing");
     }
+
 }
